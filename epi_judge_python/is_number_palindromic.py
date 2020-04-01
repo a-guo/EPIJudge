2c968a0d956e316f3f71af7343036b13db722a4c
a-guo <haoshu.guo@mapleleaf.com>
2020-04-01 17:55:28 -0400

    done primitive types

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,17 @@
 from test_framework import generic_test
-
+import math
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    if x <= 0:
+        return x == 0
+    num_digs = math.floor(math.log10(x))+1
+    mask = 10**(num_digs-1)
+    for i in range(num_digs // 2):
+        if x // mask != x % 10:
+            return False
+        x %= mask # remove most sig fig
+        x //= 10 # remove least sig fig
+        mask //= 100
     return True
 
 
